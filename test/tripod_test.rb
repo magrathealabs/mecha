692fc428fc794f226bab27e137a87e3f01426048
Alan Fachini <alfakini@gmail.com>
2017-02-19 23:18:08 -0300

    Add tests

--- test/tripod_test.rb
+++ test/tripod_test.rb
@@ -5,7 +5,11 @@ class TripodTest < Minitest::Test
     refute_nil ::Tripod::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_that_it_has_a_ruby_version_number
+    refute_nil ::Tripod::RUBY_VERSION
+  end
+
+  def test_that_it_has_a_rails_version_number
+    refute_nil ::Tripod::RAILS_VERSION
   end
 end
